55ca2854625979417ac7b06dab2b55139f751156
ValerioGoretti <46030446+ValerioGoretti@users.noreply.github.com>
2023-01-09 16:20:00 +0100

    remove node

--- Node/Data consumption/TrustedApplication/json-c-build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /volume/sgx-project/hello-enclave/json-c/apps
-# Build directory: /volume/sgx-project/hello-enclave/json-c-build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
