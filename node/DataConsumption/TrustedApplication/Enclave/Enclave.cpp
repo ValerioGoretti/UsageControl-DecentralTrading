be62b1ff2f5e8664a8bf4956bb2372f6d964edb3
dave0909 <60829979+dave0909@users.noreply.github.com>
2023-01-12 17:53:55 +0100

    Temporal rule

--- node/DataConsumption/TrustedApplication/Enclave/Enclave.cpp
+++ node/DataConsumption/TrustedApplication/Enclave/Enclave.cpp
@@ -6,7 +6,6 @@
 #include <string.h>
 #include "sgx_tprotected_fs.h"
 
-
 /*
 ===================== FILE MANAGEMENT =====================
 Leggere bene https://www.intel.com/content/www/us/en/developer/articles/technical/overview-of-intel-protected-file-system-library-using-software-guard-extensions.html 
@@ -77,9 +76,7 @@ bool enforce_geographical(){
     size_t length;
     get_geo_location(loc, sizeof(loc));
 	return strcmp(loc,"IT")==0;
-
 }
-
 
 
 int enforce_domain(){
@@ -91,26 +88,27 @@ int enforce_access_counter(){
     return 0;
 }
 
+
 bool enforce_temporal(){
     char tm[128];
 	int a;
     get_time(&a, sizeof(a));
 	snprintf(tm,128,"%d",a);
 	ocall_print(tm);
-    return 0;
+	return a<0;
 }
 
-
 SGX_FILE* access_protected_resource(const char* pub_k, const char* encr_pubk,int* id_res){
+
+
 	const char* usage_policies_filename = "Usage_Policies.txt";
 	const char* usage_logs_filename = "Usage_Logs.txt";
 	const char* mode = "w+";
-
 	SGX_FILE*  usage_policies =sgx_fopen_auto_key(usage_policies_filename, mode);
 	SGX_FILE* usage_logs =sgx_fopen_auto_key(usage_logs_filename, mode);
-	if(!enforce_geographical()) {ocall_print("Geographical position not allowed");return NULL;}
 
-	enforce_temporal();
+	if(!enforce_geographical()) {ocall_print("Geographical rule not fulfilled.");return NULL;}
+	if(!enforce_temporal()) {ocall_print("Temporal rule not fulfilled.");return NULL;}
 	//enforce_domain();
 	//enforce_access_counter();
 
